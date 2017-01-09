de6f34876639a994294cca00b4dfe6379c3a04a3
David Bolin <davidbolin@gmail.com>
2017-01-09 11:55:54 +0100

    update fisher tests

--- tests/testthat/test.fisher.R
+++ tests/testthat/test.fisher.R
@@ -95,29 +95,48 @@ test_that("Fisher, Gaussian random effects", {
 
   Sigma <- matrix(c(0.02,0,0,0.01),2,2)
   sd_Y = 2
-  Ff <-Fr <- Frf <- 0
+  Ff <- 0
+  Fr <- matrix(0,2,2)
+  Frf <- matrix(0,2,1)
   set.seed(seed)
-  d2s <- dsbf <- dsbr <- 0
+  FS <- diag(c(0,0,0))
+  FSs <- FSbf <- c(0,0,0)
+  FSbr <- matrix(0,nrow = 3, ncol = 2)
   for(i in 1:n.pers)
   {
     B_fixed[[i]]  <- as.matrix(rep(1, n.obs[i]))
     B_random[[i]] <- cbind((1:n.obs[i])/n.obs[i],((1:n.obs[i])/n.obs[i])^(2))
 
     Y[[i]] <- rnorm(n = n.obs[i], B_fixed[[i]]%*%betaf, sd = sd_Y) + B_random[[i]]%*%mvrnorm(n = 1,mu = betar, Sigma = Sigma)
-    Q =  B_random[[i]]%*%Sigma%*%t(B_random[[i]]) + sd_Y^2*diag(n.obs[i])
-    Ff <-  Ff  + t(B_fixed[[i]] )%*%solve(Q,B_fixed[[i]])
-    Fr <- Fr + t(B_random[[i]])%*%solve(Q,B_random[[i]])
-    Frf <- Frf + t(B_random[[i]])%*%solve(Q,B_fixed[[i]])
+    Q =  solve(B_random[[i]]%*%Sigma%*%t(B_random[[i]]) + sd_Y^2*diag(n.obs[i]))
+    Ff  <- Ff  + t(B_fixed[[i]] )%*%Q%*%B_fixed[[i]]
+    Fr  <- Fr  + t(B_random[[i]])%*%Q%*%B_random[[i]]
+    Frf <- Frf + t(B_random[[i]])%*%Q%*%B_fixed[[i]]
 
     v = Y[[i]] - B_fixed[[i]]%*%betaf - B_random[[i]]%*%betar
     dl = (-sd_Y*sum(diag(Q)) + sd_Y*t(v)%*%Q%*%Q%*%v)
-    d2s = d2s - (dl/sd_Y + 2*sd_Y^2*sum(diag(Q%*%Q)) - 4*sd_Y^2**t(v)%*%Q%*%Q%*%Q%*%v)
-    dsbf = dsbf + (2*sd_Y)*t(v)%*%Q%*%Q%*%B_fixed[[i]]
-    dsbr = dsbr + (2*sd_Y)*t(v)%*%Q%*%Q%*%B_random[[i]]
+    Fs = Fs - (dl/sd_Y + 2*sd_Y^2*sum(diag(Q%*%Q)) - 4*sd_Y^2**t(v)%*%Q%*%Q%*%Q%*%v)
+    Fsbf = Fsbf + (2*sd_Y)*t(v)%*%Q%*%Q%*%B_fixed[[i]]
+    Fsbr = Fsbr + (2*sd_Y)*t(v)%*%Q%*%Q%*%B_random[[i]]
+
+    vvv = matrix(c(1,0,0,0,1,0,0,0,0,0,0,1),3,4)
+    for(k in 1:3){
+      dSigma = Sigma <- matrix(vvv[k,],2,2)
+      dQS = B_random[[i]]%*%dSigma%*%t(B_random[[i]]) + sd_Y^2*diag(n.obs[i])
+      dQs = B_random[[i]]%*%Sigma%*%t(B_random[[i]]) + 2*sd_Y*diag(n.obs[i])
+      FS[k,k] = FS[k,k] - (0.5*sum(diag(-Q%*%dQS%*%Q%*%dQS)) - t(v)%*%Q%*%dQS%*%Q%*%dQS%*%Q%*%v)
+      FSs[k] = FSs[k] - (0.5*sum(diag(-Q%*%dQs%*%Q%*%dQS)) - t(v)%*%Q%*%dQs%*%Q%*%dQS%*%Q%*%v - t(v)%*%Q%*%dQS%*%Q%*%dQs%*%Q%*%v)
+      FSbf[k] = FSbf[k] + 0.5*t(v)%*%Q%*%dQS%*%Q%*%B_fixed[[i]]
+      FSbr[k,] = FSbr[k,] + 0.5*t(v)%*%Q%*%dQS%*%Q%*%B_random[[i]]
+    }
+
   }
 
-  Fe <- rBind(cBind(Ff,t(Frf)),cBind(Frf,Fr))
-  F = rBind(cBind(Fe,c(dsbf,dsbr)),c(dsbf,dsbr,d2s))
+  F <- rBind(c(Ff,t(Frf),FSbf,Fsbf),
+             cBind(Frf,Fr,t(FSbr),t(Fsbr)),
+             cBind(FSbf,FSbr,FS,FSs),
+             c(Fsbf,Fsbr,FSs,Fs))
+
 
   mixedEffect_list  <- list(B_random = B_random,
                             B_fixed  = B_fixed,
@@ -140,11 +159,10 @@ test_that("Fisher, Gaussian random effects", {
                     seed = seed,
                     nBurnin = nBurnin,
                     estimate_fisher = TRUE)
-  #print(F)
-  #print(res$FisherMatrix[c(1:3,7),c(1:3,7)])
+  print(F)
+  print(res$FisherMatrix)
 
   expect_equal(max(abs(F/res$FisherMatrix-1)),0,tolerance=0.01)
-
 })
 
 
