986b18a1b820489c28eff1d067a66098f84311e6
user-Bilgisayar\user <ozgurasarstat@gmail.com>
2017-07-05 17:25:19 +0200

    rGIG update

--- R/rGIG.R
+++ R/rGIG.R
@@ -1,10 +1,19 @@
-##
-# Internal file for simulating gig random variables in R.
-# GIG(p, a, b)
-# @param p vector of p parameters 
-# @param a vector of a parameters
-# @param b vector of b parameters
-# @param seed setting own random seed
+#' @title Simulate generalised inverse-Gaussian (GIG) random variables.
+#'
+#' @description A function to simulate random numbers 
+#'     from GIG distribution -- GIG(p, a, b).
+#' @param p A numeric vector for p parameters.
+#' @param a A numeric vector for a parameters.
+#' @param b A numeric vector for b parameters.
+#' @param seed A numeric value for setting the seed of random number generation.
+#' @details This function is a user-friendly wrapper that calls 
+#'     the \code{rGIG_cpp} function. WE NEED PDF OF GIG(p, a, b).
+#' @return A list of outputs.
+#' @examples
+#'   \dontrun{
+#'   rGIG(...)
+#'   }
+
 rGIG <- function (p, a, b, seed = 0) 
 {
   if (length(a) != length(p)) 
