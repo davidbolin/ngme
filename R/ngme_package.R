bccddcabe6ba3f53d2d1e8afc1a9a8575743b2d0
user-Bilgisayar\user <ozgurasarstat@gmail.com>
2017-08-01 09:57:15 +0200

    package .R file: "ngme" to "ngme_package"

---
+++ R/ngme_package.R
@@ -0,0 +1,11 @@
+
+#' ngme: An R package for non-Gaussian longitudinal data analysis
+#' 
+#' The ngme package provides functions for parameter estimation and prediction.
+#' 
+#' @section Function list: ...
+#' 
+#' @docType package
+#' @name ngme
+#'
+NULL
