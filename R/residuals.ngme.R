f78b6ba7729c14e38dc7ff77065fb06efc18d861
user-Bilgisayar\user <ozgurasarstat@gmail.com>
2017-08-03 10:15:15 +0200

    "residuals.nglda_est" to "residuals.ngme"

--- R/residuals.ngme.R
+++ R/residuals.ngme.R
@@ -1,23 +1,23 @@
 #' @title Residual values.
 #'
-#' @description Calculates residual values.
+#' @description Calculates residuals.
 #'
-#' @param object A fitted object by calling \code{"summary.nglda_est"}.
+#' @param object A fitted object returned by the \code{"summary.ngme"} function.
 #' @param type A character string for the type of fitted values;
 #'   \code{"marginal"} for fixed effects.
 #' @param ... Additional arguments; none used currently.
 #'
 #' @details Subject-specific residaul values are not available currently.
-#' @seealso \code{\link{nglda_est}}, \code{\link{fitted.nglda_est}}
+#' @seealso \code{\link{ngme}}, \code{\link{fitted.ngme}}
 #'
 #'
 #' @examples
 #'   \dontrun{
-#'   fit <- nglda_est(...)
+#'   fit <- ngme(...)
 #'   fitted(fit)
 #'   }
 #'
-residuals.nglda_est <- function(object, type = "marginal", ...){
+residuals.ngme <- function(object, type = "marginal", ...){
 
   if(type == "marginal"){
     resid_marg <- as.numeric(unlist(object$Y) - fitted(object, type = "marginal", ...))
