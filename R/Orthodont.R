ed2335834ff1baed32020d94140f7c36dc50121e
?zg?r Asar <ozgurasarstat@gmail.com>
2018-08-30 17:07:06 +0300

    add Orthodont

---
+++ R/Orthodont.R
@@ -0,0 +1,14 @@
+#'
+#' Orthodont data
+#'
+#' A data-set that contains dental data for 27 children at ages 8, 10, 12 and 14
+#'
+#' @format A data frame with 108 rows and 5 columns:
+#' \describe{
+#' \item{distance}{outcome variable}
+#' \item{age}{age of the child}
+#' \item{Subject}{id as character}
+#' \item{Sex}{sex of the child}
+#' \item{id}{id as numeric}
+#' }
+"Orthodont"
